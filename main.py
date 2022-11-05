1b80974365ec58bafa75ed97df388f4a84d7a97f
William Kang <contact@williamk.ga>
2022-11-05 19:11:48 -0400

    initial test

--- main.py
+++ main.py
@@ -1,28 +1,96 @@
-from LinkedList import LinkedList
+import time
+import random
 from Array import Array
 from Stack import Stack
+from LinkedList import LinkedList
 
-list = [1, 2, 5, 7, 8, 3, 8, "a", "b", "8", 2, "f", 34.3, 75.2, "g", 22]
+print("Array:\n======")
+print("Insert Start:")
+startTime = time.process_time()
+myArr = Array(10000)
+for i in range(10000):
+  myArr.insert(i, 0)
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
 
-linked = LinkedList()
-for item in list:
-    linked.insertEnd(item)
-print("\nLinked List:", linked)
-linked.insert(3, "inserted")
-linked.insertStart("start")
-linked.insertEnd("end")
-print(linked)
+print("Insert End:")
+startTime = time.process_time()
+myArr = Array(10000)
+for i in range(10000):
+  myArr.insert(i, len(myArr))
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
 
-array = Array(len(list))
-for i in range(len(list)):
-    array[i] = list[i]
-print("\nArray:", array)
-array.insert(3, "inserted")
-print(array)
+print("Random Access:")
+myLen = len(myArr)
+startTime = time.process_time()
+for i in range(10000):
+  myArr[random.randint(0, myLen-1)]
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
 
-stack = Stack()
-for item in list:
-    stack.push(item)
-print("\nStack:", stack)
-print(stack.pop(), stack)
-print(stack.get(4), stack)
+print("Random Insert:")
+startTime = time.process_time()
+for i in range(10000):
+  myArr.insert(i, random.randint(0, myLen))
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
+
+print("\nStack:\n======")
+print("Push:")
+startTime = time.process_time()
+myStack = Stack()
+for i in range(10000):
+  myStack.push(i)
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
+
+print("Pop:")
+startTime = time.process_time()
+for i in range(10000):
+  myStack.pop()
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
+
+print("Random Access:")
+myStack = Stack()
+for i in range(10000):
+  myStack.push(i)
+myLen = len(myStack)
+startTime = time.process_time()
+for i in range(10):
+  myStack.get(random.randint(0,myLen-1))
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
+
+print("\nLinked List:\n============")
+print("Insert Start:")
+startTime = time.process_time()
+myLL = LinkedList()
+for i in range(10000):
+  myLL.insertStart(i)
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
+
+print("Insert End:")
+startTime = time.process_time()
+myLL = LinkedList()
+for i in range(10000):
+  myLL.insertEnd(i)
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
+
+print("Random Access:")
+myLen = len(myLL)
+startTime = time.process_time()
+for i in range(10000):
+  myLL.find(random.randint(0, myLen-1))
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
+
+print("Random Insert:")
+startTime = time.process_time()
+for i in range(10):
+  myLL.insert(i, random.randint(0, myLen-1))
+endTime = time.process_time()
+print("Elapsed Time:", endTime-startTime)
